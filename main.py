42cacf50ac11bd259f24a99a95e8226edbc59408
Liza <kuznecovaElizaveta2006@yandex.ru>
2025-10-13 15:41:44 +0300

    base visualize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import graphviz
 
 
 def get_dependencies():
@@ -19,9 +20,23 @@ def get_dependencies():
         return {}
 
 
+def visualize(dependencies):
+    try:
+        dot = graphviz.Digraph(comment="Dependencies")
+        dot.node("E", "Express.js")
+        for name in dependencies:
+            dot.node(name, name)
+            dot.edge("E", name)
+        dot.render("dependencies.gv", view=True)
+        print("Визуализация создана")
+    except Exception as e:
+        print(f"Ошибка: {e}")
+
+
 if __name__ == "__main__":
     deps = get_dependencies()
     if deps:
         print(f"\n Получено {len(deps)} зависимостей")
+        visualize(deps)
     else:
         print("\n Не удалось получить зависимости")
